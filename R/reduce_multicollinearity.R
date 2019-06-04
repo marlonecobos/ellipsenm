b24f368956eef79f1c38f288f2a8b46a48315c2e
Marlon E. Cobos <manubio13@gmail.com>
2019-06-04 11:39:55 -0500

    V 0.1.3 helper functions added to reduce_multicollinearity

--- R/reduce_multicollinearity.R
+++ R/reduce_multicollinearity.R
@@ -1,4 +1,5 @@
-reduce_multicollinearity  <- function(data, raster_layers, sample_size = 10000,
+reduce_multicollinearity  <- function(data, longitude, latitude,
+                                      raster_layers, sample_size = 10000,
                                       correlation_limit = 0.8, VIF_limit = 10) {
   # -----------
   # detecting potential errors a
@@ -7,7 +8,7 @@ reduce_multicollinearity  <- function(data, raster_layers, sample_size = 10000,
   }
 
   if (class(raster_layers)[1] == "RasterStack") {
-    if (dim(raster_layers)[3]) {
+    if (dim(raster_layers)[3] < 2) {
       stop("raster_layers must contain at least 2 layers.")
     }
   } else {
@@ -15,27 +16,63 @@ reduce_multicollinearity  <- function(data, raster_layers, sample_size = 10000,
   }
 
   # -----------
-  # raster processing
-  rvalues <- na.omit(raster::values(raster_layers))
+  # raster processing and sampling raster data
+  raster_values <- na.omit(raster::values(raster_layers))
 
-  if (nrow(rvalues) > sample_size) {
-    rvalues <- rvalues[sample(nrow(rvalues), sample_size), ] # to get the sample
+  if (nrow(raster_values) > sample_size) {
+    raster_values <- raster_values[sample(nrow(raster_values), sample_size), ]
   }
 
   # -----------
-  # correlation matrix and groups
-  correlations <- cor(rvalues)
-  rule <- correlations > correlation_limit | correlations < -correlation_limit
-  correlations <-
-  correlations <-
-  n_cor_groups
+  # correlation groups
+
 
   # -----------
   # VIF groups
+
+
+  # -----------
+  # return object
+  groups <- list(
+    corelation_results = n_cor_groups,
+    VIF_results = list(totally_collinear = al_var,
+                       selected_variables = sel_vars)
+    )
+
+
+  return()
+}
+
+#' Helper function to find non-correlated groups of variables
+#'
+
+noncor_groups <- function(raster_values, correlation_limit) {
+  correlations <- cor(raster_values)
+  rule <- correlations > correlation_limit | correlations < -correlation_limit
+  variables <- colnames(rule)
+
+  cor_n <- sort(sapply(variables, function(x) {sum(rule[, x]) - 1}))
+  cor_l <- lapply(unique(cor_n), function(x) {names(cor_n[cor_n == x])})
+  names(cor_l) <- unique(cor_n)
+
+  var1 <- names(cor_n[cor_n == 1])
+  n_cor_groups
+}
+
+
+#' Helper function to find non-collinear groups of variables based on VIF
+#'
+
+vif_groups <- function(data, longitude, latitude, raster_layers,
+                       raster_values = NULL) {
   d_r <- na.omit(raster::extract(raster_layers, data[, c(longitude, latitude)]))
 
+  if (is.null(raster_values)) {
+    raster_values <- na.omit(raster::values(raster_layers))
+  }
+
   d_f <- as.data.frame(rbind(cbind(presence = 1, d_r),
-                             cbind(presence = 0, rvalues)))
+                             cbind(presence = 0, raster_values)))
 
   form <- paste0("presence ~ ", paste0("I(", colnames(d_f)[-1], ")"))
 
@@ -58,15 +95,4 @@ reduce_multicollinearity  <- function(data, raster_layers, sample_size = 10000,
 
   ## selected variables
   sel_vars <- vifs[vifs < VIF_limit]
-
-  # -----------
-  # return object
-  groups <- list(
-    corelation_results = n_cor_groups,
-    VIF_results = list(totally_collinear = al_var,
-                       selected_variables = sel_vars)
-    )
-
-
-  return()
 }
