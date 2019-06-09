2c58cd3d1f710a931ddad08af11080ca3b6efeb0
Marlon E. Cobos <manubio13@gmail.com>
2019-06-09 11:47:17 -0500

    V 0.1.3 documentation overlap data

--- R/spdata_overlap.R
+++ R/spdata_overlap.R
@@ -1,6 +1,35 @@
 #' Prepare species and environmental data for overlap analysis
 #'
-#' @description
+#' @description spdata_overlap prepares species and environmental data to perform
+#' niche overlap analyses.
+#'
+#' @param method (character) method to contruct the ellipsoid that characterizes
+#' the species ecological niche. Available methods are: "covmat" and "mve".
+#' See details. Default = "covmat".
+#' @param data data.frame of occurrence records. Columns must be species,
+#' longitude, and latitude.
+#' @param raster_layers a RasterStack of environmental data to be used in niche
+#' overlap ananlyses.
+#'
+#' @details
+#' Methods details are as follows:
+#'
+#' "covmat"
+#'
+#' "mve"
+#'
+#' @export
+#'
+#' @examples
+#' # data
+#' met <- "covmat"
+#' occurrences <- read.csv(system.file("extdata", "occurrences_thin.csv",
+#'                                     package = "ellipsenm"))
+#' layers <- raster::raster(system.file("extdata", "m_bio1.tif",
+#'                                      package = "ellipsenm"))
+#'
+#' sp_over <- spdata_overlap(method = met, data = occurrences,
+#'                           raster_layers = layers)
 
 spdata_overlap <- function(method, data, raster_layers) {
 
