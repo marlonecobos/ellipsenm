b7bda360984bd1b62a5c51231a8f86f6aec6e99f
Marlon E. Cobos <manubio13@gmail.com>
2019-06-11 22:03:54 -0500

    V 0.1.3 convex area related files modified

--- R/convex_area.R
+++ R/convex_area.R
@@ -12,17 +12,9 @@
 #' a number (for K-means clustering) is used to separate distinct chunks of
 #' data Recommended when the species of interest has a disjunct distribution.
 #' Default = FALSE.
-#' @param cluster_method (character) name of the method to be used for clustering
-#' the occurrences. Options are "hierarchical" and "k-means"; default =
-#' "hierarchical". Note that this parameter is ignored when \code{split} = FALSE.
-#' See details \code{\link[ellipsenm]{cluster_split}}.
-#' @param split_distance (numeric) distance in km that will be considered as the
-#' limit of connectivity among polygons created with clusters of occurrences.
-#' This parameter is used when \code{cluster_method} = "hierarchical" and
-#' \code{split} = TRUE. Default = NULL.
 #' @param n_kmeans (numeric) if \code{split} = TRUE, number of clusters in which
-#' the species occurrences will be grouped when using the "k-means"
-#' \code{cluster_method}. Default = NULL.
+#' the species occurrences will be grouped using the "k-means" method.
+#' Default = NULL.
 #' @param buffer_distance (numeric) distance in km to be used to create a buffer
 #' for the convex hull. Default = NULL.
 #' @param raster_layers optional RasterStack to be used in restricting the
@@ -62,18 +54,20 @@
 #'                         latitude = "LATITUDE", buffer_distance = 50)
 #'
 #' # producing convex polygons splitted considering clusters
-#'
+#' cx_area2 <- convex_area(data = occs, longitude = "LONGITUDE",
+#'                         latitude = "LATITUDE", split = TRUE, n_kmeans = 3,
+#'                         buffer_distance = 50)
 #'
 #' # producing convex polygons, masking layers
 #' vars <- raster::stack(list.files(system.file("extdata", package = "ellipsenm"),
 #'                                  pattern = "m_bio", full.names = TRUE))
 #'
-#' cx_area <- convex_area(data = occs, longitude = "LONGITUDE",
-#'                         latitude = "LATITUDE", buffer_distance = 50,
+#' cx_area3 <- convex_area(data = occs, longitude = "LONGITUDE",
+#'                         latitude = "LATITUDE", buffer_distance = 100,
 #'                         raster_layers = vars, mask = TRUE)
 #'
 #' # producing convex polygons, masking layers, and saving results
-#' cx_area3 <- convex_area(data = occurrences, longitude = "longitude",
+#' cx_area4 <- convex_area(data = occurrences, longitude = "longitude",
 #'                        latitude = "latitude", buffer_distance = 100,
 #'                        raster_layers = vars, mask = TRUE, save = TRUE,
 #'                        name = "buff_area")
@@ -108,7 +102,7 @@ convex_area <- function(data, longitude, latitude, split = FALSE,
   # -----------
   # Defining clusters if needed
   if (split == TRUE) {
-    occ_sp <- cluster_split(occ_sp, n_kmeans)
+    occ_sp <- cluster_split(occ_sp, n_kmeans = n_kmeans)
   } else {
     occ_sp@data <- data.frame(occ_sp@data, clusters = 1)
   }
@@ -235,7 +229,8 @@ convex_area <- function(data, longitude, latitude, split = FALSE,
 #'
 #' @export
 
-cluster_split <- function(data, n_kmeans = NULL) {
+cluster_split <- function(data, cluster_method = "k-means",
+                          split_distance = 250, n_kmeans = NULL) {
 
   if (cluster_method == "hierarchical" | cluster_method == "k-means") {
     # split groups of points based on the split distance
