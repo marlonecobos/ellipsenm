2d339c0bc06a027d867a1516ae6d50eadac63ac1
Marlon E. Cobos <manubio13@gmail.com>
2019-06-14 14:01:43 -0500

    V 0.1.3 ellipse_volumen added

--- R/ellipsoid_fit.R
+++ R/ellipsoid_fit.R
@@ -1,4 +1,5 @@
-ellipsoid_fit <- function (data, mve = TRUE, level, vars = NULL) {
+ellipsoid_fit <- function (data, method = "mve", level, vars = NULL) {
+
   if (is.null(vars)) {
     nvars <- readline(prompt = "Number of variables to fit the ellipsoid model:\\n\\n")
     data <- data.frame(data)
@@ -6,6 +7,7 @@ ellipsoid_fit <- function (data, mve = TRUE, level, vars = NULL) {
     print(nvars)
     vars <- numeric(nvars)
     cat("Select a variable form the list:\\n\\n")
+
     for (i in 1:dim(data)[2]) {
       cat(i, ".-", allvars[i], "\\n")
     }
@@ -22,7 +24,7 @@ ellipsoid_fit <- function (data, mve = TRUE, level, vars = NULL) {
     }
   }
   data <- data[, vars]
-  if (mve) {
+  if (method = "mve") {
     NDquntil <- function(nD, level) {
       n <- floor(nD * level)
       if (n > nD)
@@ -33,43 +35,51 @@ ellipsoid_fit <- function (data, mve = TRUE, level, vars = NULL) {
     cent_var <- MASS::cov.mve(data, quantile.used = n)
     centroid <- cent_var$center
     vari <- cent_var$cov
-  }
-  else {
+  } else {
     centroid <- colMeans(data)
     vari <- stats::cov(data)
   }
-  ellip_vol <- function(n, axis_length) {
-    term1 <- 2 * pi^(n/2)
-    term2 <- n * gamma(n/2)
-    term3 <- prod(axis_length)
-    term4 <- (term1/term2) * term3
-    return(term4)
-  }
-  sigmaI <- solve(vari)/stats::qchisq(level, df = dim(data)[2])
+
+  sigmaI <- solve(vari) / stats::qchisq(level, df = dim(data)[2])
   sigIEigenSys <- eigen(sigmaI)
   sigIEval <- sigIEigenSys$values
   sigIEvec <- sigIEigenSys$vectors
-  stds <- 1/sqrt(sigIEval)
+  stds <- 1 / sqrt(sigIEval)
   axis_length <- NULL
+
   for (i in 1:dim(sigmaI)[1]) {
     axis_length[i] <- stds[i] * 2
   }
+
   names(axis_length) <- letters[1:dim(vari)[1]]
   n <- dim(vari)[1]
-  vol2 <- ellip_vol(n, axis_length/2)
+  vol2 <- ellip_vol(n, axis_length / 2)
   axis_coordinates <- list()
+
   for (i in 1:dim(vari)[1]) {
-    assign(paste0("l", i, "_inf"), centroid - sigIEvec[,
-                                                       i] * stds[i])
-    assign(paste0("l", i, "_sup"), centroid + sigIEvec[,
-                                                       i] * stds[i])
-    coord_matrix <- matrix(c(eval(parse(text = paste0("l",
-                                                      i, "_sup"))), eval(parse(text = paste0("l", i, "_inf")))),
+    assign(paste0("l", i, "_inf"), centroid - sigIEvec[, i] * stds[i])
+    assign(paste0("l", i, "_sup"), centroid + sigIEvec[, i] * stds[i])
+    coord_matrix <- matrix(c(eval(parse(text = paste0("l", i, "_sup"))),
+                             eval(parse(text = paste0("l", i, "_inf")))),
                            byrow = T, nrow = 2)
     colnames(coord_matrix) <- names(centroid)
     rownames(coord_matrix) <- paste0("vec_", 1:2)
     axis_coordinates[[i]] <- coord_matrix
   }
+
   return(list(centroid = centroid, covariance = vari, niche_volume = vol2,
               SemiAxis_length = axis_length/2, axis_coordinates = axis_coordinates))
+}
+
+
+#' Helper function to calculate niche volume
+#'
+
+ellip_vol <- function (n, axis_length) {
+  term1 <- 2 * pi^(n / 2)
+  term2 <- n * gamma(n/2)
+  term3 <- prod(axis_length)
+  term4 <- (term1 / term2) * term3
+
+  return(term4)
 }
