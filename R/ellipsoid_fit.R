631baaf99836f9711f74f8b893e64aad79711322
Marlon E. Cobos <manubio13@gmail.com>
2019-06-14 21:19:31 -0500

    V 0.1.3 ellipse_fit helper functions added

--- R/ellipsoid_fit.R
+++ R/ellipsoid_fit.R
@@ -1,37 +1,14 @@
-ellipsoid_fit <- function (data, method = "mve", level, vars = NULL) {
+ellipsoid_fit <- function (data, longitude, latitude, method = "mve", level,
+                           raster_layers = NULL) {
 
-  if (is.null(vars)) {
-    nvars <- readline(prompt = "Number of variables to fit the ellipsoid model:\\n\\n")
-    data <- data.frame(data)
-    allvars <- names(data)
-    print(nvars)
-    vars <- numeric(nvars)
-    cat("Select a variable form the list:\\n\\n")
-
-    for (i in 1:dim(data)[2]) {
-      cat(i, ".-", allvars[i], "\\n")
-    }
-    cont <- 1
-    while (0 %in% vars) {
-      n <- readline(prompt = "Enter an option from the above list: ")
-      if (n %in% 1:dim(data)[2]) {
-        vars[cont] <- as.numeric(n)
-        cont <- cont + 1
-      }
-      else {
-        cat("Option not in the list:\\n")
-      }
-    }
+  if (!is.null(raster_layers)) {
+    data <- raster::extract(raster_layers, data[, c(longitude, latitude)])
+  } else {
+    data <- data[, -c(longitude, latitude)]
   }
-  data <- data[, vars]
+
   if (method = "mve") {
-    NDquntil <- function(nD, level) {
-      n <- floor(nD * level)
-      if (n > nD)
-        n <- nD
-      return(n)
-    }
-    n <- NDquntil(dim(data)[1], level)
+    n <- ndata_quantile(nrow(data), level)
     cent_var <- MASS::cov.mve(data, quantile.used = n)
     centroid <- cent_var$center
     vari <- cent_var$cov
@@ -40,25 +17,25 @@ ellipsoid_fit <- function (data, method = "mve", level, vars = NULL) {
     vari <- stats::cov(data)
   }
 
-  sigmaI <- solve(vari) / stats::qchisq(level, df = dim(data)[2])
-  sigIEigenSys <- eigen(sigmaI)
-  sigIEval <- sigIEigenSys$values
-  sigIEvec <- sigIEigenSys$vectors
-  stds <- 1 / sqrt(sigIEval)
+  sigma_i <- solve(vari) / stats::qchisq(level, df = dim(data)[2])
+  s_eigen <- eigen(sigma_i)
+  s_eigenval <- s_eigen$values
+  s_eigenvec <- s_eigen$vectors
+  stds <- 1 / sqrt(s_eigenval)
   axis_length <- NULL
 
-  for (i in 1:dim(sigmaI)[1]) {
+  for (i in 1:dim(sigma_i)[1]) {
     axis_length[i] <- stds[i] * 2
   }
 
   names(axis_length) <- letters[1:dim(vari)[1]]
   n <- dim(vari)[1]
-  vol2 <- ellip_vol(n, axis_length / 2)
+  vol2 <- ellipsoid_volume(n, axis_length / 2)
   axis_coordinates <- list()
 
   for (i in 1:dim(vari)[1]) {
-    assign(paste0("l", i, "_inf"), centroid - sigIEvec[, i] * stds[i])
-    assign(paste0("l", i, "_sup"), centroid + sigIEvec[, i] * stds[i])
+    assign(paste0("l", i, "_inf"), centroid - s_eigenvec[, i] * stds[i])
+    assign(paste0("l", i, "_sup"), centroid + s_eigenvec[, i] * stds[i])
     coord_matrix <- matrix(c(eval(parse(text = paste0("l", i, "_sup"))),
                              eval(parse(text = paste0("l", i, "_inf")))),
                            byrow = T, nrow = 2)
@@ -75,11 +52,20 @@ ellipsoid_fit <- function (data, method = "mve", level, vars = NULL) {
 #' Helper function to calculate niche volume
 #'
 
-ellip_vol <- function (n, axis_length) {
+ellipsoid_volume <- function (n, axis_length) {
   term1 <- 2 * pi^(n / 2)
-  term2 <- n * gamma(n/2)
+  term2 <- n * gamma(n / 2)
   term3 <- prod(axis_length)
   term4 <- (term1 / term2) * term3
 
   return(term4)
+}
+
+
+#' Helper function to calculate quantiles
+
+ndata_quantile <- function(n_data, level) {
+  n <- floor(n_data * level)
+  if (n > n_data) {n <- n_data}
+  return(n)
 }
