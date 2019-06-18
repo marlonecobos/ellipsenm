c6ea1be39fd85b683a6be568f23f6a62a7035c6f
Marlon E. Cobos <manubio13@gmail.com>
2019-06-17 19:07:40 -0500

    V 0.1.3 slots changed

--- R/all_classes.R
+++ R/all_classes.R
@@ -1,40 +1,40 @@
 # R classes for ellipsenm objects
 # May 2019
-# Version 0.0.1
+# Version 0.1.3
 # Licence GPL v3
 
-ellipsoid_model <- setClass("ellipsoid_model",
-                            slots = c(method = "character",
-                                      occ_predictions = "data.frame",
-                                      centroid = "numeric",
-                                      covariance_matrix = "matrix",
-                                      niche_volume = "numeric",
-                                      semi_axis_length = "numeric",
-                                      axis_coordinates = "list",
-                                      predictions = "RasterLayer",
-                                      projections = "list"))
+ellipsoid_mod <- setClass("ellipsoid_model",
+                          slots = c(method = "character",
+                                    occ_predictions = "data.frame",
+                                    centroid = "numeric",
+                                    covariance_matrix = "matrix",
+                                    niche_volume = "numeric",
+                                    semi_axes_length = "numeric",
+                                    axis_coordinates = "list",
+                                    predictions = "RasterLayer",
+                                    projections = "list"))
 
 ellipsoid <- setClass("ellipsoid",
                       slots = c(method = "character",
                                 centroid = "numeric",
                                 covariance_matrix = "matrix",
                                 niche_volume = "numeric",
-                                semi_axis_length = "numeric",
+                                semi_axes_length = "numeric",
                                 axis_coordinates = "list"))
 
-ellipsoid_calibration <- setClass("ellipsoid_calibration",
-                                  slots = c(centroid_types = "character",
-                                            covariance_types = "character",
-                                            variable_sets = "list",
-                                            results = "data.frame"))
+ellipsoid_cal <- setClass("ellipsoid_calibration",
+                          slots = c(centroid_types = "character",
+                                    covariance_types = "character",
+                                    variable_sets = "list",
+                                    results = "data.frame"))
 
 data_overlap <- setClass("data_overlap",
                          slots = c(method = "character",
                                    occurrences = "data.frame",
                                    m_layers = "RasterStack"))
 
-ellipsoid_overlap <- setClass("ellipsoid_overlap",
-                              slots = c(method = "character",
-                                        data = "list",
-                                        models = "list",
-                                        overlap = "data.frame"))
+ellipsoid_ove <- setClass("ellipsoid_overlap",
+                          slots = c(method = "character",
+                                    data = "list",
+                                    models = "list",
+                                    overlap = "data.frame"))
