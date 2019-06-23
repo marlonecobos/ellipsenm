e9b647c168e51d35333f65b0d5143e71203aeba3
Marlon E. Cobos <manubio13@gmail.com>
2019-06-23 13:25:11 -0500

    V 0.1.6 classes updated

--- R/all_classes.R
+++ R/all_classes.R
@@ -1,31 +1,45 @@
 # R classes for ellipsenm objects
 # May 2019
-# Version 0.1.3
+# Version 0.1.5
 # Licence GPL v3
 
-ellipsoid_mod <- setClass("ellipsoid_model",
-                          slots = c(data_predictions = "data.frame",
-                                    prediction = "RasterLayer",
-                                    projections = "list"),
-                          contains = "ellipsoid_fit")
-
-ellipsoid <- setClass("ellipsoid_fit",
+ellipsoid <- setClass("ellipsoid",
                       slots = c(method = "character",
                                 centroid = "numeric",
                                 covariance_matrix = "matrix",
                                 level = "numeric",
                                 niche_volume = "numeric",
                                 semi_axes_length = "numeric",
-                                axis_coordinates = "list"))
+                                axes_coordinates = "list"))
 
-ellipsoid_cal <- setClass("ellipsoid_calibration",
-                          slots = c(methods = "character",
-                                    data = "data.frame",
-                                    variable_sets = "list",
-                                    level = "numeric",
-                                    results = "data.frame",
-                                    selection_criteria = "character",
-                                    selected_parameters = "character"))
+ellipsoid_model_sim <- setClass("ellipsoid_model_sim",
+                              slots = c(mahalanobis = "numeric",
+                                        suitability = "numeric",
+                                        prevalence = "numeric",
+                                        prediction_maha = "RasterLayer",
+                                        prediction_suit = "RasterLayer",
+                                        projections_maha = "character",
+                                        projections_suit = "character"),
+                              contains = "ellipsoid")
+
+ellipsoid_model_rep <- setClass("ellipsoid_model_rep",
+                              slots = c(ellipsoids = "list",
+                                        mahalanobis = "matrix",
+                                        suitability = "matrix",
+                                        prevalence = "matrix",
+                                        prediction_maha = "RasterStack",
+                                        prediction_suit = "RasterStack",
+                                        projections_maha = "list",
+                                        projections_suit = "list"))
+
+calibration_ellipsoid <- setClass("calibration_ellipsoid",
+                                  slots = c(methods = "character",
+                                            data = "data.frame",
+                                            variable_sets = "list",
+                                            level = "numeric",
+                                            results = "data.frame",
+                                            selection_criteria = "character",
+                                            selected_parameters = "character"))
 
 data_overlap <- setClass("data_overlap",
                          slots = c(method = "character",
@@ -33,8 +47,8 @@ data_overlap <- setClass("data_overlap",
                                    raster_layers = "RasterStack",
                                    level = "numeric"))
 
-ellipsoid_ove <- setClass("ellipsoid_overlap",
-                          slots = c(method = "character",
-                                    data = "list",
-                                    models = "list",
-                                    overlap = "data.frame"))
+overlap_ellipsoid <- setClass("overlap_ellipsoid",
+                              slots = c(method = "character",
+                                        data = "list",
+                                        models = "list",
+                                        overlap = "data.frame"))
