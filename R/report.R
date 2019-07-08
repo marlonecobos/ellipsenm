69cd8ef43bb6ff74734abb5f6d1c5c3c22397395
Marlon E. Cobos <manubio13@gmail.com>
2019-07-08 17:12:33 -0500

    V 0.2.1 reporting depending on prediction and projection

--- R/report.R
+++ R/report.R
@@ -4,9 +4,13 @@
 #' the main processes that the ellipsenm package performs.
 #'
 #' @param repor_type (character) type of report to be produced. Options are:
-#' "calibration", "enm_suitability", "enm_mahalanobis", "enm_both", and "overlap".
-#' @param name (character) name of the HTML file to be produced, file extention
-#' does not need to be defined.
+#' "calibration", "enm", and "overlap".
+#' @param prediction (character) if report_type = "enm". Type of prediction that
+#' was performed, oprions are: "suitability", "mahalanobis", and "both".
+#' @param projected (logical) if report_type = "enm", whether or not the model
+#' was projected to distinct scenarios.
+#' @param output_directory (character) name of the directory where the HTML file
+#' will be produced. Default = working directory.
 #'
 #' @return
 #' An HTML file summarizing results from the main processes that the ellipsenm
@@ -17,58 +21,95 @@
 #' a css file with format instructions should exist. See \code{\link{report_format}}.
 #'
 #' @export
-#'
-#' @examples
-#' # for results from modeling processes
-#' report(report_type = "enm_suitability", name = "enm_suitability_report")
 
-report <- function(report_type, name = "results_report") {
+report <- function(report_type, prediction, projected, output_directory = getwd()) {
   # -----------
-  # detecting potential errors and preparing name
+  # detecting potential errors
   if (missing(report_type)) {
     stop("Argument report_type is missing.")
   }
-  name1 <- name
-  name <- paste0(name, ".Rmd")
+  if (!report_type %in% c("calibration", "enm", "overlap")) {
+    stop("Argument report_type is not valid, please see function's help.")
+  }
+  if (report_type == "enm") {
+    if (missing(prediction)) {
+      stop("Argument prediction is missing.")
+    }
+    if (!prediction %in% c("mahalanobis", "both", "suitability")) {
+      stop("Argument prediction is not valid, please see function's help.")
+    }
+    if (missing(projected)) {
+      stop("Argument projected is missing.")
+    }
+  }
 
   # -----------
   # preparing report
-  if (report_type %in% c("calibration", "enm_suitability", "enm_mahalanobis",
-                       "enm_both", "overlap")) {
+  # report types for enm
+  if (report_type == "enm") {
+    if (projected == TRUE) {
+      if (prediction != "mahalanobis") {
+        if (prediction == "both") {
+          name <- paste0(output_directory, "/enm_both_pr_report.Rmd")
+          suppressMessages(
+            file.copy(from = system.file("extdata", "enm_both_pr_report.Rmd",
+                                         package = "ellipsenm"), to = name)
+          )
+        } else {
+          name <- paste0(output_directory, "/enm_suitability_pr_report.Rmd")
+          suppressMessages(
+            file.copy(from = system.file("extdata", "enm_suitability_pr_report.Rmd",
+                                         package = "ellipsenm"), to = name)
+          )
+        }
+      } else {
+        name <- paste0(output_directory, "/enm_mahalanobis_pr_report.Rmd")
+        suppressMessages(
+          file.copy(from = system.file("extdata", "enm_mahalanobis_pr_report.Rmd",
+                                       package = "ellipsenm"), to = name)
+        )
+      }
+    } else {
+      if (prediction != "mahalanobis") {
+        if (prediction == "both") {
+          name <- paste0(output_directory, "/enm_both_report.Rmd")
+          suppressMessages(
+            file.copy(from = system.file("extdata", "enm_both_report.Rmd",
+                                         package = "ellipsenm"), to = name)
+          )
+        } else {
+          name <- paste0(output_directory, "/enm_suitability_report.Rmd")
+          suppressMessages(
+            file.copy(from = system.file("extdata", "enm_suitability_report.Rmd",
+                                         package = "ellipsenm"), to = name)
+          )
+        }
+      } else {
+        name <- paste0(output_directory, "/enm_mahalanobis_report.Rmd")
+        suppressMessages(
+          file.copy(from = system.file("extdata", "enm_mahalanobis_report.Rmd",
+                                       package = "ellipsenm"), to = name)
+        )
+      }
+    }
+  }
 
-    if (report_type == "calibration") {
-      suppressMessages(
-        file.copy(from = system.file("extdata", "enm_calibration_report.Rmd",
-                                     package = "ellipsenm"), to = name)
-      )
-    }
-    if (report_type == "enm_suitability") {
-      suppressMessages(
-        file.copy(from = system.file("extdata", "enm_suitability_report.Rmd",
-                                     package = "ellipsenm"), to = name)
-      )
+  # report for calibration
+  if (report_type == "calibration") {
+    name <- paste0(output_directory, "/enm_calibration_report.Rmd")
+    suppressMessages(
+      file.copy(from = system.file("extdata", "enm_calibration_report.Rmd",
+                                   package = "ellipsenm"), to = name)
+    )
+  }
 
-    }
-    if (report_type == "enm_mahalanobis") {
-      suppressMessages(
-        file.copy(from = system.file("extdata", "enm_mahalanobis_report.Rmd",
-                                     package = "ellipsenm"), to = name)
-      )
-    }
-    if (report_type == "enm_both") {
-      suppressMessages(
-        file.copy(from = system.file("extdata", "enm_both_report.Rmd",
-                                     package = "ellipsenm"), to = name)
-      )
-    }
-    if (report_type == "overlap") {
-      suppressMessages(
-        file.copy(from = system.file("extdata", "niche_overlap_report.Rmd",
-                                     package = "ellipsenm"), to = name)
-      )
-    }
-  } else {
-    stop("Argument report_type is not valid, please see function's help.")
+  # report type for overlap
+  if (report_type == "overlap") {
+    name <- paste0(output_directory, "/niche_overlap_report.Rmd")
+    suppressMessages(
+      file.copy(from = system.file("extdata", "niche_overlap_report.Rmd",
+                                   package = "ellipsenm"), to = name)
+    )
   }
 
   # -----------
@@ -77,6 +118,6 @@ report <- function(report_type, name = "results_report") {
 
   # -----------
   # reporting
-  cat(paste0("\nA file named ", paste0(name1, ".html"), " has been produced.\n",
-             "Check your output directory in:\t", getwd()))
+  cat(paste0("\nA file named ", paste0(gsub(".Rmd$", "", name), ".html"),
+             " has been produced.\n", "Check your output directory in:\t", getwd()))
 }
