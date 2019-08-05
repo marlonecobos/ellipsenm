80656474554a339e91597ec4590ac6df0f6dad16
Marlon E. Cobos <manubio13@gmail.com>
2019-08-05 13:50:54 -0500

    V 0.2.4 partial_ROC working with raster and numeric and all helpers

--- R/partial_roc.R
+++ R/partial_roc.R
@@ -1,165 +1,162 @@
-#' Partial ROC calculation of single models
+#' Partial ROC calculation for ecological niche models
 #'
-#' @description kuenm_proc applies partial ROC tests to single models.
+#' @description partial_roc applies partial ROC tests to model predictions.
 #'
-#' @param occ.test a numerical matrix containing coordinates of the occurrences used to test
-#' the ecological niche model to be evaluated; columns must be: longitude and latitude.
-#' @param model a RasterLayer of the ecological niche model to be evaluated.
-#' @param threshold (numeric) value from 0 to 100 that will be used as threshold (E);
-#' default = 5.
-#' @param rand.percent (numeric) value from 0 to 100 representing the percent of testing data
-#' to be used for performing the bootstrap process for calculating the partial ROC;
-#' default = 50.
+#' @param prediction RasterLayer or numeric vector of ecological niche model
+#' predictions to be evaluated. If RasterLayer, layer of predicted suitability.
+#' If numeric vector, predicted suitability values.
+#' @param test_data matrix, data.frame, or numeric vector containing coordinates
+#' of occurrences to test model predictions to be evaluated. If matrix or
+#' data.frame, columns must include longitude and latitude to extract values of
+#' raster prediction. If numeric, values of suitability in such occurrences. If
+#' a matrix or a data.frame is provided, \code{prediction} must be a RasterLayer.
+#' @param error (numeric) value from 0 to 100 to represent the percentage of
+#' potential error (E) that the data could have due to any source of uncertainty.
+#' Default = 5.
 #' @param iterations (numeric) number of bootstrap iterations to be performed;
 #' default = 500.
-#' @param parallel (logical) if TRUE, calculations will be performed in parallel using the available
-#' cores of the computer. This will demand more RAM and almost full use of the CPU; hence, its use
-#' is more recommended in high-performance computers. Using this option will speed up the analyses
-#' only if \code{model} is a large RasterLayer or if \code{iterations} are more than 5000.
-#' Default = FALSE.
+#' @param percentage (numeric) percentage of testing data to be used in each
+#' bootstrapped process for calculating the partial ROC. Default = 50.
 #'
-#' @return A data.frame containing the AUC values and AUC ratios calculated for each iteration.
+#' @return A list with the summary of the results and a data.frame containing
+#' the AUC values and AUC ratios calculated for all iterations.
 #'
-#' @details Partial ROC is calculated following Peterson et al.
-#' (2008; \url{http://dx.doi.org/10.1016/j.ecolmodel.2007.11.008}). This function is a modification
-#' of the \code{\link[ntbox]{pROC}} funcion, available at \url{https://github.com/luismurao/ntbox}.
+#' @details Partial ROC is calculated following Peterson et al. (2008;
+#' \url{http://dx.doi.org/10.1016/j.ecolmodel.2007.11.008}).
 #'
 #' @importFrom purrr map_df
-#' @useDynLib kuenm
+#' @useDynLib ellipsenm
 #' @export
 #'
 #' @examples
-#' occ <- read.csv(list.files(system.file("extdata", package = "kuenm"),
-#'                            pattern = "sp_test.csv", full.names = TRUE))
-#' model <- raster::raster(list.files(system.file("extdata", package = "kuenm"),
-#'                                    pattern = "sp_model.tif", full.names = TRUE))
-#' thres <- 5
-#' rand_perc <- 50
-#' iterac <- 500
+#' occurrences <- read.csv(system.file("extdata", "occurrences.csv",
+#'                         package = "ellipsenm"))
+#' prediction <- raster::raster(system.file("extdata", "prediction.tif",
+#'                                       package = "ellipsenm"))
 #'
-#' p_roc <- kuenm_proc(occ.test = occ, model = model, threshold = thres,
-#'                    rand.percent = rand_perc, iterations = iterac)
+#' p_roc <- partial_roc(prediction = prediction, test_data = occurrences,
+#'                      longitude = "Longitude", latitude = "Latitude")
 
-kuenm_proc <- function(occ.test, model, threshold = 5, rand.percent = 50,
-                       iterations = 500, parallel = FALSE){
+partial_roc <- function(prediction, test_data, longitude, latitude, error = 5,
+                        iterations = 500, percentage = 50) {
+
+  # -----------
+  # detecting potential errors, other potential problems tested in code
+  if (missing(prediction)) {
+    stop("Argument prediction is necessary to perform the analysis.")
+  }
+  if (missing(test_data)) {
+    stop("Argument occurrences is necessary to perform the analysis.")
+  }
+  c_pred <- class(prediction)[1]
+  if (!c_pred %in% c("RasterLayer", "numeric")) {
+    stop("prediction must be of class RasterLayer or numeric.")
+  }
+  c_tdat <- class(test_data)[1]
+  if (!c_tdat %in% c("matrix", "data.frame", "numeric")) {
+    stop("test_data must be of class matrix, data.frame, or numeric.")
+  }
+  if (c_pred == "numeric" & c_tdat != "numeric") {
+    stop("test_data must be of class numeric if prediction is a numeric vector.")
+  }
+
+  # -----------
+  # package and function needed
   suppressPackageStartupMessages(library(dplyr))
-  suppressPackageStartupMessages(library(future))
 
-  if(model@data@min == model@data@max){
-    warning("\nModel with no variability, pROC will return NA.\n")
+  calc_aucDF <- function(big_classpixels, fractional_area, test_data, n_data,
+                         n_samp, error_sens) {
+    rowsID <- sample(x = n_data, size = n_samp, replace = TRUE)
+    test_data1 <- test_data[rowsID]
+    omssion_matrix <- big_classpixels > test_data1
+    sensibility <- 1 - colSums(omssion_matrix) / n_samp
+    xyTable <- data.frame(fractional_area, sensibility)
+    less_ID <- which(xyTable$sensibility <= error_sens)
+    xyTable <- xyTable[-less_ID, ]
+    xyTable <- xyTable[order(xyTable$fractional_area, decreasing = F), ]
+    auc_pmodel <- ellipsenm:::trap_roc(xyTable$fractional_area, xyTable$sensibility)
+    auc_prand <- ellipsenm:::trap_roc(xyTable$fractional_area, xyTable$fractional_area)
+    auc_ratio <- auc_pmodel / auc_prand
+    auc_table <- data.frame(auc_pmodel, auc_prand, auc_ratio = auc_ratio )
+    return(auc_table)
+  }
+
+  # -----------
+  # preparing data
+  min_pred <- ifelse(c_pred == "numeric", min(prediction, na.rm = TRUE),
+                     prediction@data@min)
+  max_pred <- ifelse(c_pred == "numeric", max(prediction, na.rm = TRUE),
+                     prediction@data@max)
+
+  prediction <- round((prediction / max_pred) * 1000)
+  if (c_pred == "RasterLayer") {
+    if (c_tdat != "numeric") {
+      test_data <- na.omit(raster::extract(prediction,
+                                           test_data[, c(longitude, latitude)]))
+    } else {
+      test_data <- round((test_data / max_pred) * 1000)
+    }
+    classpixels <- data.frame(raster::freq(prediction, useNA = "no"))
+  } else {
+    test_data <- round((test_data / max_pred) * 1000)
+    vals <- na.omit(unique(prediction))
+    classpixels <- data.frame(value = vals, count = c(table(prediction)),
+                              row.names = 1:length(vals))
+  }
+
+  # -----------
+  # analysis
+  if(min_pred == max_pred){
+    warning("\nprediction has no variability, pROC will return NA.\n")
 
     p_roc <- rep(NA, 2)
-    names(p_roc) <- c(paste("Mean_AUC_ratio_at_", threshold, "%", sep = ""), "pval_pROC")
+    names(p_roc) <- c(paste0("Mean_AUC_ratio_at_", error, "%"), "pval_pROC")
 
     auc_ratios <- rep(NA, 3)
-    names(auc_ratios) <- c("Model_partial_AUC", "Random_curve_partial_AUC", "AUC_ratio")
+    names(auc_ratios) <- c("Prediction_partial_AUC", "Random_curve_partial_AUC",
+                           "AUC_ratio")
 
     p_roc_res <- list(pROC_summary = p_roc, pROC_results = auc_ratios)
 
     return(p_roc_res)
   }else {
-    model <- round((model/raster::cellStats(model, max)) * 1000)
-    test_value <- na.omit(raster::extract(model, occ.test))
-    #test_value <- unique(test_value)
-    classpixels <- data.frame(raster::freq(model))
-    classpixels <- data.frame(stats::na.omit(classpixels))
-
-    classpixels <- classpixels  %>% dplyr::mutate_(value= ~rev(value),
-                                                   count= ~rev(count),
-                                                   totpixperclass = ~cumsum(count),
-                                                   percentpixels= ~ totpixperclass/sum(count)) %>%
+    classpixels <- classpixels %>%
+      dplyr::mutate_(value = ~rev(value),
+                     count = ~rev(count),
+                     totpixperclass = ~cumsum(count),
+                     percentpixels = ~totpixperclass/sum(count)) %>%
       dplyr::arrange(value)
 
 
-    error_sens <- 1-(threshold/100)
-    models_thresholds <- classpixels[,"value"]
-    fractional_area <- classpixels[,"percentpixels"]
-    n_data <- length(test_value)
-    n_samp <- ceiling((rand.percent/100)*(n_data))
+    error_sens <- 1 - (error / 100)
+    prediction_errors <- classpixels[, "value"]
+    fractional_area <- classpixels[, "percentpixels"]
+    n_data <- length(test_data)
+    n_samp <- ceiling((percentage / 100) * n_data)
 
-    big_classpixels <- matrix(rep(models_thresholds,each=n_samp),
-                              ncol=length(models_thresholds))
+    big_classpixels <- matrix(rep(prediction_errors, each = n_samp),
+                              ncol = length(prediction_errors))
 
+    partial_AUC <- 1:iterations %>%
+      purrr::map_df(~calc_aucDF(big_classpixels, fractional_area, test_data,
+                                n_data, n_samp, error_sens))
 
-    calc_aucDF <- function(big_classpixels,fractional_area,
-                           test_value,n_data,n_samp,error_sens){
+    naID <- !is.na(partial_AUC$auc_ratio)
+    nona_valproc <- partial_AUC$auc_ratio[naID]
+    mauc <- mean(nona_valproc)
+    proc <- (sum(nona_valproc <= 1) + sum(nona_valproc)) / length(nona_valproc)
+    valid_iter <- length(nona_valproc)
 
-      rowsID <- sample(x = n_data,
-                       size = n_samp,
-                       replace=TRUE)
-      test_value1 <- test_value[rowsID]
-      omssion_matrix <-   big_classpixels >  test_value1
-      sensibility <- 1 - colSums(omssion_matrix)/n_samp
-      xyTable <- data.frame(fractional_area,sensibility)
-      less_ID <- which(xyTable$sensibility<=error_sens)
-      xyTable <- xyTable[-less_ID,]
-
-      xyTable <- xyTable[order(xyTable$fractional_area,
-                               decreasing = F),]
-
-      auc_pmodel <- trap_roc(xyTable$fractional_area,
-                             xyTable$sensibility)
-
-      auc_prand <- trap_roc(xyTable$fractional_area,
-                            xyTable$fractional_area)
-      auc_ratio <- auc_pmodel/auc_prand
-
-      auc_table <- data.frame(auc_pmodel,
-                              auc_prand,
-                              auc_ratio =auc_ratio )
-      return(auc_table)
-
-    }
-
-
-    if(parallel){
-
-      future::plan(future::multiprocess)
-      roc_env <- new.env()
-      n_cores <- future::availableCores()
-      niter_big <- floor(iterations/n_cores)
-      n_runs <- rep(niter_big,n_cores)
-      sum_n_runs <- sum(n_runs)
-      n_runs[1] <- n_runs[1] + (iterations - sum_n_runs)
-
-      for(i in 1:length(n_runs)){
-        x <- as.character(i)
-        roc_env[[x]] %<-% {
-          x1 <- 1:n_runs[i]
-          auc_matrix1 <- x1 %>%
-            purrr::map_df(~calc_aucDF(big_classpixels,
-                                      fractional_area,
-                                      test_value,n_data,n_samp,
-                                      error_sens))
-        }
-      }
-      partial_AUC <- as.list(roc_env)
-      rm(roc_env)
-      partial_AUC <- do.call(rbind.data.frame,partial_AUC)
-      rownames(partial_AUC) <- NULL
-      future::plan(future::sequential)
-
-    }else{
-
-      partial_AUC <- 1:iterations %>%
-        purrr::map_df(~calc_aucDF(big_classpixels,
-                                  fractional_area,
-                                  test_value,n_data,n_samp,
-                                  error_sens))
-
-    }
-
-    mauc <- mean(partial_AUC$auc_ratio, na.rm = TRUE)
-    #proc <- sum(partial_AUC$auc_ratio <= 1) / length(partial_AUC$auc_ratio)
-    proc <- (sum(partial_AUC$auc_ratio[!is.na(partial_AUC$auc_ratio)] <= 1) + sum(is.na(partial_AUC$auc_ratio)))/
-      length(partial_AUC$auc_ratio)
-    p_roc <- c(mauc, proc)
-    names(p_roc) <- c(paste("Mean_AUC_ratio_at_", threshold, "%", sep = ""), "pval_pROC")
+    p_roc <- c(mauc, proc, valid_iter)
+    names(p_roc) <- c(paste0("Mean_AUC_ratio_at_", error, "%"), "pval_pROC",
+                      "Valid_iterations")
 
     auc_ratios <- partial_AUC
-    names(auc_ratios) <- c("Model_partial_AUC", "Random_curve_partial_AUC", "AUC_ratio")
+    names(auc_ratios) <- c("Prediction_partial_AUC", "Random_curve_partial_AUC",
+                           "AUC_ratio")
 
     p_roc_res <- list(pROC_summary = p_roc, pROC_results = auc_ratios)
+
     return(p_roc_res)
   }
 }
