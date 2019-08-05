047b1561708809f294d9e2b8de84bf7540c43ea3
Marlon E. Cobos <manubio13@gmail.com>
2019-08-05 17:01:17 -0500

    V 0.2.6 package documentation

---
+++ R/ellipsenm.R
@@ -0,0 +1,21 @@
+#' ellipsenm: An R package for ecological niche's characterization using ellipsoids
+#'
+#' ellipsenm implements multiple tools to help users in using ellipsoids to model
+#' ecological niches of species. Handly options for calibrating and selecting
+#' models, producing models with replicates and projections, and assessing niche
+#' overlap are included as part of this package. Other functions implemented here
+#' are useful to perform a series of pre- and post-modeling analyses.
+#'
+#'
+#' @section Main functions in ellipsenm:
+#' \code{\link{thin_data}}, \code{\link{split_ata}}, \code{\link{data_subsample}},
+#' \code{\link{buffer_area}}, \code{\link{convex_area}}, \code{\link{concave_area}},
+#' \code{\link{polygon_selection}}, \code{\link{explore_espace}},
+#' \code{\link{ellipsoid_fit}}, \code{\link{ellipsoid_calibration}},
+#' \code{\link{predict}}, \code{\link{ellipsoid_model}},
+#' \code{\link{model_projection}}, \code{\link{partial_roc}},
+#' \code{\link{omission_rate}}
+#'
+#' @docType package
+#' @name ellipsenm
+NULL
